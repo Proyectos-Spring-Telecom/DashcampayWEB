03be02436506903f98182a1344dd7150dce0398f
Proyectos-Spring-Telecom <proyectospring.telecom@gmail.com>
2025-11-14 12:58:39 -0600

    [Add]Vista Pasajero

--- src/app/pages/administracion/perfil-pasajero/perfil-pasajero.component.ts
+++ src/app/pages/administracion/perfil-pasajero/perfil-pasajero.component.ts
@@ -11,5 +11,35 @@ import { fadeInRight400ms } from '@vex/animations/fade-in-right.animation';
 export class PerfilPasajeroComponent {
 
   layoutCtrl = new UntypedFormControl('fullwidth');
+  mesActual = 'Noviembre 2025';          // o lo calculas dinámico
+gastoMes = 12345;                      // número
+saldoActual = 56789.34;                // número
+ultimaRecarga: Date = new Date();      // fecha de la última recarga
+
+
+movimientos = [
+  {
+    tipo: 'Debito',
+    concepto: 'Pago de servicio',
+    descripcion: 'Luz | Cargo automático',
+    monto: 405.34,
+    fecha: new Date(2020, 0, 20)
+  },
+  {
+    tipo: 'Debito',
+    concepto: 'Depósito',
+    descripcion: 'Transferencia bancaria',
+    monto: 1200,
+    fecha: new Date(2020, 0, 18)
+  },
+  {
+    tipo: 'Recarga',
+    concepto: 'Recarga',
+    descripcion: 'DashCam Pay',
+    monto: 500,
+    fecha: new Date(2020, 0, 15)
+  }
+];
+
 
 }
